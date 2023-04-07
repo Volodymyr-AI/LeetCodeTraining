f1a1d713cf3358d93e72c68ba9acaa1369701d9b
Volodymyr Moiseichenko <alternativeqoob@gmial.com>
2023-04-07 20:50:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCodeTraining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
